4fbc2f8f0df61d9a3094c0326ee03bee5cfe34e5
Cameron Bisca <cjbisca@gmail.com>
2020-11-03 20:41:31 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -7,3 +7,20 @@ def loop_message_five_times(message)
     count += 1 
   end
 end
+
+def loop_message_n_times(message, limit)
+  count = 0 
+  while count < limit do 
+    puts message 
+    count += 1 
+  end 
+end 
+
+def output_array(array) 
+  count = 0 
+  while count < array.length do 
+    puts array[count]
+    count += 1 
+  end 
+end 
+
